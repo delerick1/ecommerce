08f4ac144d04f3d54aebfd7441b5684c9e4cae0d
Delerick1 <Erick134545@gmail.com>
2022-05-02 02:36:18 -0500

    all most done

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -9,3 +9,29 @@ function menutog(){
         MenuItems.style.maxHeight = "0px"
     }
 }
+
+
+
+var ProductImg = document.getElementById("Product-Img");
+var SmallImg = document.getElementsByClassName("small-img");
+
+
+
+
+SmallImg[0].onclick = function()
+{
+    ProductImg.src = SmallImg[0].src;
+  
+}
+SmallImg[1].onclick = function()
+{
+    ProductImg.src = SmallImg[1].src;
+}
+SmallImg[2].onclick = function()
+{
+    ProductImg.src = SmallImg[2].src;
+}
+SmallImg[3].onclick = function()
+{
+    ProductImg.src = SmallImg[3].src;
+}
